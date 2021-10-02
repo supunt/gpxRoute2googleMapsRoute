9ec67a2ee3ef88fc04fd5fc37a426533225024dc
Supun De Silva <supun1001@gmail.com>
2021-10-02 16:36:10 +1000

    Adding few doc links
    Signed-off-by: Supun De Silva <supun1001@gmail.com>

--- server.ts
+++ server.ts
@@ -11,7 +11,9 @@ import { existsSync } from 'fs';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/gpx-to-google-maps/browser');
+  let exec_path = process.cwd()
+
+  const distFolder = join(exec_path, 'dist/gpx-to-google-maps/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
